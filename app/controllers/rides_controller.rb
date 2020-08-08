343d832e664b1daaeec7c38f8a5f62f268b79b08
github username <github email address>
2020-08-08 15:01:07 -0700

    Done.

---
+++ app/controllers/rides_controller.rb
@@ -0,0 +1,25 @@
+class RidesController < ApplicationController
+  before_action :ride
+
+
+    def ride 
+       # byebug
+        @attraction=Attraction.find(params[:ride][:attraction_id]) 
+        ride=Ride.new(:user_id => current_user.id,:attraction_id => @attraction.id)
+        ride=ride.take_ride
+        flash[:aaa]=ride
+
+    redirect_to user_path(current_user)  
+  end
+
+    def create 
+
+    end
+
+
+
+
+    def show
+
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :rides
+  resources :attractions
+  root "application#hello"
+  resources :users
+
+
+  get '/signin' => 'session#new'
+  post '/signin' => 'session#signin'
+  delete '/' => 'users#destroy'
+  post '/rides' => 'rides#ride'
 end
