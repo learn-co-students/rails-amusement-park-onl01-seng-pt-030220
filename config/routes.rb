4bb1dd3471757aa3e072da15e36220f6de3b30d6
MMcClure11 <mcclme02@luther.edu>
2020-08-11 09:02:51 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :attractions
+  resources :rides
+
+  root 'users#home'
+
+  get '/signin', to: 'sessions#new', as: :signin
+  post '/signin', to: "sessions#create"
+  get '/signout', to: "sessions#destroy", as: :signout
+
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
